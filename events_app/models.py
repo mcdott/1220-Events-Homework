89193d63ed6d1491c9744839be5823fb7287827b
mcdott <111994803+mcdott@users.noreply.github.com>
2023-01-24 19:17:02 -0500

    Created the model classes for Guest and Event and the guest_event table

--- events_app/models.py
+++ events_app/models.py
@@ -11,6 +11,10 @@ from sqlalchemy.orm import backref
 
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.String(12), nullable=False)
+    events_attending = db.relationship('Event', secondary='guest_event_table', back_populates='guests')
 
 # TODO: Create a model called `Event` with the following fields:
 # - id: primary key
@@ -24,9 +28,18 @@ class Guest(db.Model):
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description: db.Column(db.String(280))
+    date_and_time: db.Column(db.DateTime)
+    guests = db.relationship('Guest', secondary='guest_event_table', back_populates='events_attending')
+    event_type = db.Column(db.Enum("Party", "Study", "Networking", "Lecture", "Workshop", name="event_type"))
+
 
 # TODO: Create a table `guest_event_table` with the following columns:
 # - event_id: Integer column (foreign key)
 # - guest_id: Integer column (foreign key)
 
-guest_event_table = None
+guest_event_table = db.Table('guest_event',
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True),
+    db.Column('guest_id', db.Integer, db.ForeignKey('guest.id'), primary_key=True)
+)
